2ef32d6b3454764ad29b222ac49a41f97d9822ca
adie <webmaster@adintr.com>
2024-12-16 17:42:42 +0800

    'input_num'

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,21 @@ use std::env;
 use std::io;
 use std::io::Write;
 
-/*
-use std::any::type_name;
-
-fn print_type_of<T>(_: T) {
-    println!("type is {}", std::any::type_name::<T>())
-}
-*/
-
-fn inputNum() -> i32 {
-    let mut buffer = String::new();
-    if io::stdin().read_line(&mut buffer).is_err() {
-        eprintln!("读取输入失败");
-        return 0;
+fn input_num() -> i32 {
+    loop
+    {
+        let mut line = String::new();        
+        io::stdin().read_line(&mut line).unwrap();
+        let num: i32 = match line.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                print!("only interger input allowed: ");
+                io::stdout().flush().unwrap();
+                continue;
+            }
+        };
+        return num;        
     }
-
-    let num: i32 = match buffer.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            eprintln!("请输入一个有效的整数");
-            return 0;
-        }
-    };
-
-    return num;
 }
 
 fn main() {
@@ -37,7 +28,7 @@ fn main() {
     let conf = Ini::load_from_file(ini_file).unwrap();
     let i = 1;
 
-    for (sec, prop) in &conf {
+    for (sec, _prop) in &conf {
         if sec.is_none() {
             continue;
         }
@@ -47,6 +38,6 @@ fn main() {
 
     print!("Please select a game:");
     io::stdout().flush().unwrap();
-    let sel = inputNum();
+    let sel = input_num();
     println!("select {}", sel);
 }
